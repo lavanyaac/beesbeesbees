adb68b4a78eaf0e9809501e1e422c3a8b074ed8f
Hack Reactor Student <student@hackreactor.com>
2017-04-03 15:19:26 -0700

    Completed beesbeesbees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+	Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+
+  // Grub.apply(this, [5, 'yellow']);
+
+  this.job = 'keep on growing';
 };
+
+Bee.prototype = Object.create(Grub.prototype)
+Bee.prototype.constructor = Bee;
 
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,15 @@
+// var Grub = function(age = 0, color = 'pink', food = 'jelly') {
 var Grub = function() {
+  // this.age = age;
+  // this.color = color;
+  // this.food = food;
+
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+
 };
 
+Grub.prototype.eat = function() {
+
+};
